00990ff5183c525a01d6f45a9c61223e829efc8b
RRJJ <rj19990728@gmail.com>
2021-06-14 23:16:00 +0800

    更改點擊為xml dataBinding

--- app/src/main/java/tw/edu/pu/nihongo_benkyo/ui/game/GameSettingFragment.kt
+++ app/src/main/java/tw/edu/pu/nihongo_benkyo/ui/game/GameSettingFragment.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import tw.edu.pu.nihongo_benkyo.R
 import tw.edu.pu.nihongo_benkyo.databinding.FragmentGameSettingBinding
-import java.util.ArrayList
 
 class GameSettingFragment : Fragment() {
     private lateinit var gameViewModel: GameViewModel
@@ -23,54 +21,38 @@ class GameSettingFragment : Fragment() {
     ): View? {
         gameViewModel = ViewModelProvider(this).get(GameViewModel::class.java)
         dataBinding = FragmentGameSettingBinding.inflate(inflater, container, false)
-        //取資料
-        dataBinding.type.setOnClickListener {
-            gameViewModel.single = true
-            dataBinding.recycler.setBackgroundColor(
-                ContextCompat.getColor(
-                    requireContext(),
-                    R.color.pink_800
-                )
-            )
-        }
-
-        dataBinding.testContent.setOnClickListener {
-            gameViewModel.single = false
-            dataBinding.recycler.setBackgroundColor(
-                ContextCompat.getColor(
-                    requireContext(),
-                    R.color.pink_500
-                )
-            )
-        }
-        dataBinding.next.setOnClickListener {
-            val bundle = Bundle()
-            bundle.putString("type", gameViewModel.type)
-            bundle.putStringArrayList("tags", gameViewModel.selectTag as ArrayList<String>)
-            when(gameViewModel.type ){
-                "選擇題" -> findNavController().navigate(R.id.action_nav_game_to_gamingSelectionFragment, bundle)
-                "" -> Toast.makeText(requireContext(), "有選擇沒選到喔", Toast.LENGTH_LONG).show()
-                else -> findNavController().navigate(R.id.action_nav_game_to_gamingInputFragment, bundle)
-            }
-
-        }
-
         return dataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         dataBinding.lifecycleOwner = activity
-        gameViewModel.tags.observe(viewLifecycleOwner, {
-            adapter = GameSettingAdapter(requireActivity(), gameViewModel, gameViewModel.single)
+        gameViewModel.single.postValue(true)
+        gameViewModel.single.observe(viewLifecycleOwner, {
+            adapter = GameSettingAdapter(requireActivity(), gameViewModel, it)
             dataBinding.adapter = adapter
-            adapter.setTagData(it)
-        })
+            if (it){
+                dataBinding.recycler.setBackgroundColor(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        R.color.pink_500
+                    )
+                )
+                // get tag Data from database
+                //adapter.setTagData()
+            }
+            else{
+                dataBinding.recycler.setBackgroundColor(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        R.color.pink_800
+                    )
+                )
+                // get type Data from database
+                //adapter.setTypeData()
+            }
 
-        gameViewModel.types.observe(viewLifecycleOwner, {
-            adapter = GameSettingAdapter(requireActivity(), gameViewModel, gameViewModel.single)
-            dataBinding.adapter = adapter
-            adapter.setTypeData(it)
+
         })
 
     }
--- app/src/main/java/tw/edu/pu/nihongo_benkyo/ui/game/GameViewModel.kt
+++ app/src/main/java/tw/edu/pu/nihongo_benkyo/ui/game/GameViewModel.kt
@@ -1,15 +1,19 @@
 package tw.edu.pu.nihongo_benkyo.ui.game
 
+import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import tw.edu.pu.nihongo_benkyo.R
 import tw.edu.pu.nihongo_benkyo.model.database.Type
 import tw.edu.pu.nihongo_benkyo.model.database.Tag
 import tw.edu.pu.nihongo_benkyo.model.Repository
 import tw.edu.pu.nihongo_benkyo.model.database.Question
 import kotlin.collections.ArrayList
+import kotlin.math.sin
 
 class GameViewModel : ViewModel() {
     private val repository = Repository()
@@ -18,9 +22,9 @@ class GameViewModel : ViewModel() {
     var tags: MutableLiveData<List<Tag>> = MutableLiveData()
     var types: MutableLiveData<List<Type>> = MutableLiveData()
     var answerInput: MutableLiveData<String> = MutableLiveData()
+    var single: MutableLiveData<Boolean> = MutableLiveData()
     var type: String = ""
     var selectTag: List<String> = ArrayList()
-    var single: Boolean = true
 
     // gaming
     var index = 0
@@ -29,6 +33,24 @@ class GameViewModel : ViewModel() {
 
     init {
         index = 0
+    }
+
+    fun setting(view: View, single:Boolean){
+        this.single.postValue(single)
+    }
+
+    fun settingComplete(view: View){
+        val bundle = Bundle()
+        bundle.putString("type",type)
+        bundle.putStringArrayList("tags", selectTag as java.util.ArrayList<String>)
+        if (selectTag.isNotEmpty()){
+            when(type){
+                "選擇題" -> Navigation.findNavController(view).navigate(R.id.action_nav_game_to_gamingSelectionFragment, bundle)
+                "" -> Toast.makeText(view.context, "有選擇沒選到喔", Toast.LENGTH_LONG).show()
+                else -> Navigation.findNavController(view).navigate(R.id.action_nav_game_to_gamingInputFragment, bundle)
+            }
+        }else
+            Toast.makeText(view.context, "有選擇沒選到喔", Toast.LENGTH_LONG).show()
     }
 
     fun selectionBtnClick(view: View, select:Int){
